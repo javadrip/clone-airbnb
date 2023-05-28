06b7f2a6b811a58959d31980c2917e1e8598dcd8
Vinleon Ang <javadripdev@gmail.com>
2023-05-28 16:06:55 +0800

    Add logo

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,10 +1,15 @@
 import Container from "../Container";
+import Logo from "./Logo";
 
 export default function Navbar() {
   return (
     <div className="fixed w-full bg-white z-10 shadow-sm">
       <div className="py-4 border-b-[1px]">
-        <Container>anythign</Container>
+        <Container>
+          <div className="flex items-center justify-between gap-3 md:gap-0">
+            <Logo />
+          </div>
+        </Container>
       </div>
     </div>
   );
