05670dbfa6a6a04aa349c30fa58345a5d482ec6b
Vinleon Ang <javadripdev@gmail.com>
2023-05-31 17:22:23 +0800

    Create categories structure

---
+++ app/components/navbar/Categories.tsx
@@ -0,0 +1,55 @@
+import { useSearchParams, usePathname } from "next/navigation";
+
+import Container from "../Container";
+import CategoryBox from "../CategoryBox";
+
+import { TbBeach } from "react-icons/tb";
+import { GiWindmill } from "react-icons/gi";
+import { MdOutlineVilla } from "react-icons/md";
+
+export const categories = [
+  {
+    label: "Beach",
+    icon: TbBeach,
+    description: "This property is close to the beach!",
+  },
+  {
+    label: "Windmills",
+    icon: GiWindmill,
+    description: "This property has windmills!",
+  },
+  {
+    label: "Modern",
+    icon: MdOutlineVilla,
+    description: "This property is modern!",
+  },
+];
+
+const Categories = () => {
+  const params = useSearchParams();
+  const category = params?.get("category");
+  const pathname = usePathname();
+
+  const isMainPage = pathname === "/";
+
+  if (!isMainPage) {
+    return null;
+  }
+
+  return (
+    <Container>
+      <div className="pt-4 flex flex-row items-center justify-between overflow-x-auto">
+        {categories.map(item => (
+          <CategoryBox
+            key={item.label}
+            label={item.label}
+            icon={item.icon}
+            selected={category === item.label}
+          />
+        ))}
+      </div>
+    </Container>
+  );
+};
+
+export default Categories;
