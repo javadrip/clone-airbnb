0871038011ed6ce06f5229412cbbe3cb4eb7d38a
Vinleon Ang <javadripdev@gmail.com>
2023-06-05 14:10:00 +0800

    Create getListing action

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,32 @@
-export default function Home() {
-  return <div className="text-rose-500 text-2xl">Hello Airbnb!</div>;
+import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
+
+import getListings from "./actions/getListings";
+
+export default async function Home() {
+  const listings = await getListings();
+
+  if (listings.length === 0) {
+    return <EmptyState showReset />;
+  }
+
+  return (
+    <Container>
+      <div
+        className="
+            pt-24
+            grid
+            grid-cols-1
+            sm:grid-cols-2
+            md:grid-cols-3
+            lg:grid-cols-4
+            xl:grid-cols-5
+            2xl:grid-cols-6
+            gap-8
+          "
+      >
+        <div>Future listings</div>
+      </div>
+    </Container>
+  );
 }
