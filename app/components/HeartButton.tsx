eedefb24873d8aa6a02fa41aa8dc1d4ec2d79468
Vinleon Ang <javadripdev@gmail.com>
2023-06-05 15:56:38 +0800

    Add useFavorite hook

--- app/components/HeartButton.tsx
+++ app/components/HeartButton.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import useFavorite from "./hooks/useFavorite";
+
 import { SafeUser } from "@/app/types";
 
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
@@ -13,8 +15,10 @@ const HeartButton: React.FC<HeartButtonProps> = ({
   listingId,
   currentUser,
 }) => {
-  const hasFavorited = false;
-  const toggleFavorite = () => {};
+  const { hasFavorited, toggleFavorite } = useFavorite({
+    listingId,
+    currentUser,
+  });
 
   return (
     <div
