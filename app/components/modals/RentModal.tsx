f3375f5452db73749e022a8917270317dc58f994
Vinleon Ang <javadripdev@gmail.com>
2023-06-05 10:53:57 +0800

    Add info step

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -12,6 +12,7 @@ import { categories } from "../navbar/Categories";
 import CategoryInput from "../inputs/CategoryInput";
 import CountrySelect from "../inputs/CountrySelect";
 import dynamic from "next/dynamic";
+import Counter from "../inputs/Counter";
 
 enum STEPS {
   CATEGORY = 0,
@@ -50,6 +51,9 @@ const RentModal = () => {
 
   const category = watch("category");
   const location = watch("location");
+  const guestCount = watch("guestCount");
+  const roomCount = watch("roomCount");
+  const bathroomCount = watch("bathroomCount");
 
   // Map has to be loaded dynamically, because it uses Leaflet which is not SSR compatible
   // Ignore location warning, because we are using dynamic import and we know location is necessary
@@ -135,6 +139,39 @@ const RentModal = () => {
         />
         {/* Map is loaded dynamically, because it uses Leaflet which is not SSR compatible */}
         <Map center={location?.latlng} />
+      </div>
+    );
+  }
+
+  // =========================== Step 3: Info =========================== //
+  if (step === STEPS.INFO) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Share some information about your place"
+          subtitle="What amenities do you offer?"
+        />
+        <Counter
+          title="Guests"
+          subtitle="How many guests can your place accommodate?"
+          value={guestCount}
+          onChange={value => setCustomValue("guestCount", value)}
+        />
+        <hr />
+        <Counter
+          title="Rooms"
+          subtitle="How many rooms can guests use?"
+          value={roomCount}
+          onChange={value => setCustomValue("roomCount", value)}
+        />
+        <hr />
+        <Counter
+          title="Bathrooms"
+          subtitle="How many bathrooms can guests use?"
+          value={bathroomCount}
+          onChange={value => setCustomValue("bathroomCount", value)}
+        />
+        <hr />
       </div>
     );
   }
