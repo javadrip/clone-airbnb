95a6f105b94ed632c3e307526db79726bfe84680
Vinleon Ang <javadripdev@gmail.com>
2023-06-08 16:33:38 +0800

    Add steps and modal navigation

--- app/components/modals/SearchModal.tsx
+++ app/components/modals/SearchModal.tsx
@@ -1,11 +1,118 @@
 "use client";
 
+import { useCallback, useMemo, useState } from "react";
+import { Range } from "react-date-range";
+import { formatISO } from "date-fns";
+import dynamic from "next/dynamic";
+import { useRouter, useSearchParams } from "next/navigation";
+import qs from "query-string";
+
 import useSearchModal from "@/app/hooks/useSearchModal";
 
 import Modal from "./Modal";
+import { CountrySelectValue } from "../inputs/CountrySelect";
+
+enum STEPS {
+  LOCATION = 0,
+  DATE = 1,
+  INFO = 2,
+}
 
 const SearchModal = () => {
+  const router = useRouter();
+  const params = useSearchParams();
   const searchModal = useSearchModal();
+
+  const [step, setStep] = useState(STEPS.LOCATION);
+  const [location, setLocation] = useState<CountrySelectValue>();
+  const [guestCount, setGuestCount] = useState(1);
+  const [roomCount, setRoomCount] = useState(1);
+  const [bathroomCount, setBathroomCount] = useState(1);
+  const [dateRange, setDateRange] = useState<Range>({
+    startDate: new Date(),
+    endDate: new Date(),
+    key: "selection",
+  });
+
+  const Map = useMemo(
+    () => dynamic(() => import("../Map"), { ssr: false }),
+    [location]
+  );
+
+  const onBack = useCallback(() => {
+    setStep(value => value - 1);
+  }, []);
+
+  const onNext = useCallback(() => {
+    setStep(value => value + 1);
+  }, []);
+
+  const onSubmit = useCallback(async () => {
+    if (step !== STEPS.INFO) {
+      return onNext();
+    }
+
+    let currentQuery = {};
+
+    if (params) {
+      currentQuery = qs.parse(params.toString());
+    }
+
+    const updatedQuery: any = {
+      ...currentQuery,
+      locationValue: location?.value,
+      guestCount,
+      roomCount,
+      bathroomCount,
+    };
+
+    if (dateRange.startDate) {
+      updatedQuery.startDate = formatISO(dateRange.startDate);
+    }
+
+    if (dateRange.endDate) {
+      updatedQuery.endDate = formatISO(dateRange.endDate);
+    }
+
+    const url = qs.stringifyUrl(
+      {
+        url: "/",
+        query: updatedQuery,
+      },
+      { skipNull: true }
+    );
+
+    setStep(STEPS.LOCATION);
+    searchModal.onClose();
+    router.push(url);
+  }, [
+    step,
+    searchModal,
+    location,
+    router,
+    guestCount,
+    roomCount,
+    dateRange,
+    onNext,
+    bathroomCount,
+    params,
+  ]);
+
+  const actionLabel = useMemo(() => {
+    if (step === STEPS.INFO) {
+      return "Search";
+    }
+
+    return "Next";
+  }, [step]);
+
+  const secondaryActionLabel = useMemo(() => {
+    if (step === STEPS.LOCATION) {
+      return undefined;
+    }
+
+    return "Back";
+  }, [step]);
 
   return (
     <Modal
