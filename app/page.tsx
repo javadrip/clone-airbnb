f2fa924e39782230126dc64ab7e586d1d6a8ca36
Vinleon Ang <javadripdev@gmail.com>
2023-06-08 15:33:27 +0800

    Refactor Home component to accept searchParams as props

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,15 @@ import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
 import ListingCard from "./components/listings/ListingCard";
 
-import getListings from "./actions/getListings";
+import getListings, { IListingsParams } from "./actions/getListings";
 import getCurrentUser from "./actions/getCurrentUser";
 
-export default async function Home() {
-  const listings = await getListings();
+interface HomeProps {
+  searchParams: IListingsParams;
+}
+
+const Home = async ({ searchParams }: HomeProps) => {
+  const listings = await getListings(searchParams);
   const currentUser = await getCurrentUser();
 
   if (listings.length === 0) {
@@ -40,4 +44,6 @@ export default async function Home() {
       </div>
     </Container>
   );
-}
+};
+
+export default Home;
