c68c395697f0553280799e404643c8e5000d8d00
Vinleon Ang <javadripdev@gmail.com>
2023-06-08 14:04:47 +0800

    Add reservation

--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -2,7 +2,7 @@ import EmptyState from "@/app/components/EmptyState";
 
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import getListingById from "@/app/actions/getListingById";
-// import getReservations from "@/app/actions/getReservations";
+import getReservations from "@/app/actions/getReservations";
 
 import ListingClient from "./ListingClient";
 
@@ -12,14 +12,20 @@ interface IParams {
 
 const ListingPage = async ({ params }: { params: IParams }) => {
   const listing = await getListingById(params);
-  // const reservations = await getReservations(params);
+  const reservations = await getReservations(params);
   const currentUser = await getCurrentUser();
 
   if (!listing) {
     return <EmptyState />;
   }
 
-  return <ListingClient listing={listing} currentUser={currentUser} />;
+  return (
+    <ListingClient
+      listing={listing}
+      reservations={reservations}
+      currentUser={currentUser}
+    />
+  );
 };
 
 export default ListingPage;
