16040cae8891846a1d6552c89f93e585f7fc4e4f
Vinleon Ang <javadripdev@gmail.com>
2023-06-08 14:01:24 +0800

    Add type SafeReservation

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,7 +1,17 @@
-import { Listing, User } from "@prisma/client";
+import { Listing, Reservation, User } from "@prisma/client";
 
 export type SafeListing = Omit<Listing, "createdAt"> & {
   createdAt: string;
+};
+
+export type SafeReservation = Omit<
+  Reservation,
+  "createdAt" | "startDate" | "endDate" | "listing"
+> & {
+  createdAt: string;
+  startDate: string;
+  endDate: string;
+  listing: SafeListing;
 };
 
 // Created because in getCurrentUser.ts, we converted dates to strings, but in Navbar.tsx, the User type is used, which expects createdAt, updatedAt, and emailVerified to be Date objects. This type is used in Navbar.tsx instead of User.
